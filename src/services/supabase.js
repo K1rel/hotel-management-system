46f996e545821efca3c6ad1e3d2bdcd1406fae9f
k1rel <kiril.gerasimovski@gmail.com>
2025-01-27 21:09:15 +0100

    re-init

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,10 +1,9 @@
 /* eslint-disable no-unused-vars */
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://dudrufhbqlimxumybvan.supabase.co";
-// Create a single supabase client for interacting with your database
-const supabase = createClient(
-  "https://dudrufhbqlimxumybvan.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR1ZHJ1ZmhicWxpbXh1bXlidmFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc2OTY2NjIsImV4cCI6MjA0MzI3MjY2Mn0.EdM2UCstJAMzM4nzM7B0ewF0JUwmDSRNb4PFq8U9-S8"
-);
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
